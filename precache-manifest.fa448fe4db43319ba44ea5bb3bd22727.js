90520cfb568075f645dffcaa4d2717777c812c01
ipodtouch64g <qazwyt3196@gmail.com>
2020-05-07 00:41:09 +0800

    Updates

---
+++ precache-manifest.fa448fe4db43319ba44ea5bb3bd22727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c3c8ed426325e7c68f1816aaa9eb3a",
+    "url": "/ModernPTT/index.html"
+  },
+  {
+    "revision": "cb95257083422d1524e4",
+    "url": "/ModernPTT/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2a4c00e66d328a4524a9",
+    "url": "/ModernPTT/static/js/2.714d5156.chunk.js"
+  },
+  {
+    "revision": "b040bf30180637b59575e45a86bfd994",
+    "url": "/ModernPTT/static/js/2.714d5156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb95257083422d1524e4",
+    "url": "/ModernPTT/static/js/main.1387b5ab.chunk.js"
+  },
+  {
+    "revision": "06acccc5e4d4f8d42e61",
+    "url": "/ModernPTT/static/js/runtime-main.9b62de90.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd896ffe3d916608e6199f53805ea454.js"
+  "/ModernPTT/precache-manifest.fa448fe4db43319ba44ea5bb3bd22727.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ModernPTT/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
