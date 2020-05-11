88a42d1799d7a7ab65ee0d8437fd84ecb8d5b9e3
ipodtouch64g <qazwyt3196@gmail.com>
2020-05-11 22:32:20 +0800

    variable length comment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ModernPTT/precache-manifest.3c96ef9e2194d73d4e7cc9ebf4dcd14e.js"
+  "/ModernPTT/precache-manifest.6cb402f24b29f316ca6eb3cd6876c73c.js"
 );
 
 self.addEventListener('message', (event) => {
