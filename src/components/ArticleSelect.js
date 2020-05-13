f2205d06f536e727cf4fe0204760144ca22742c7
ipodtouch64g <qazwyt3196@gmail.com>
2020-05-13 16:23:09 +0800

    add search article

--- src/components/ArticleSelect.js
+++ src/components/ArticleSelect.js
@@ -11,9 +11,17 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Fab from "@material-ui/core/Fab";
 import SearchIcon from "@material-ui/icons/Search";
-import Grow from "@material-ui/core/Grow";
-import ArticleSearch from "./ArticleSearch";
 import Backdrop from "@material-ui/core/Backdrop";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import IconButton from "@material-ui/core/IconButton";
+import Paper from "@material-ui/core/Paper";
+import { useForm, Controller } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import { getArticleList } from "./utils/article";
 
 export default function ArticleSelect(props) {
 	const useStyles = makeStyles(theme => ({
@@ -40,8 +48,45 @@ export default function ArticleSelect(props) {
 			zIndex: "1001"
 		},
 		backdrop: {
-			zIndex: theme.zIndex.drawer + 1,
-			backgroundColor : 'unset'
+			zIndex: theme.zIndex.drawer + 1
+		},
+		paper: {
+			flexDirection: "column"
+		},
+		form: {
+			width: "40vw",
+			margin: "12px",
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center"
+		},
+		button: {
+			alignSelf: "flex-end",
+			marginTop: "12px"
+		},
+		title: {
+			alignSelf: "flex-start",
+			width: "90%",
+			margin: "12px"
+		},
+		author: {
+			alignSelf: "flex-start",
+			width: "90%",
+			margin: "12px"
+		},
+		push: {
+			alignSelf: "flex-start",
+			width: "90%",
+			margin: "12px"
+		},
+		closeIcon: {
+			position: "absolute",
+			margin: "12px"
+		},
+		saTitle: {
+			textAlign: "center",
+			margin: "24px"
 		}
 	}));
 
@@ -60,8 +105,29 @@ export default function ArticleSelect(props) {
 			setArticleItems(res);
 		}
 	}, [info]);
+
 	const handleClickSearch = () => {
 		setIsArticleSearchOpen(isArticleSearchOpen => !isArticleSearchOpen);
+	};
+	const { handleSubmit, control, errors } = useForm();
+	// console.log(errors);
+	const onSubmit = async values => {
+		console.log(values);
+		// search for matching articles
+		let criteria = {
+			boardname: info.boardName,
+			title: values.title,
+			author: values.author,
+			push: values.push
+		};
+		try {
+			let res = await getArticleList(BotContext,criteria);
+			info.setArticleList(res);
+			console.log(res)
+			handleClickSearch();
+		} catch (err) {
+			console.log(err);
+		}
 	};
 	const handleLoadMore = () => {
 		// prevent default action
@@ -89,7 +155,6 @@ export default function ArticleSelect(props) {
 			info.setArticleList([...info.articleList, ...res_reduced]);
 		});
 	};
-
 	return (
 		<Grid
 			container
@@ -107,8 +172,84 @@ export default function ArticleSelect(props) {
 					<Backdrop
 						className={classes.backdrop}
 						open={isArticleSearchOpen}
+						invisible={true}
 					>
-						<ArticleSearch className={classes.articleSearch} />
+						<ClickAwayListener
+							mouseEvent={isArticleSearchOpen ? "onClick" : false}
+							onClickAway={handleClickSearch}
+						>
+							<Paper elevation={4} className={classes.paper}>
+								<IconButton
+									className={classes.closeIcon}
+									onClick={handleClickSearch}
+								>
+									<CloseIcon />
+								</IconButton>
+
+								<Typography className={classes.saTitle}>
+									搜尋文章
+								</Typography>
+
+								<Divider
+									className={classes.divider}
+									orientation="horizontal"
+								/>
+								<form
+									onSubmit={handleSubmit(onSubmit)}
+									className={classes.form}
+									noValidate
+									autoComplete="off"
+								>
+									<Controller
+										className={classes.title}
+										as={<TextField label="標題" />}
+										name="title"
+										control={control}
+										defaultValue=""
+									/>
+
+									<Controller
+										className={classes.author}
+										as={<TextField label="作者" />}
+										name="author"
+										control={control}
+										defaultValue=""
+									/>
+
+									<Controller
+										className={classes.push}
+										as={
+											<TextField
+												label="推數(大於多少)"
+												helperText={
+													errors.length > 0
+														? "必須是數字"
+														: ""
+												}
+												error={errors.length > 0}
+											/>
+										}
+										rules={{
+											pattern: {
+												value: /[0-9]*/
+											}
+										}}
+										name="push"
+										control={control}
+										defaultValue=""
+									/>
+
+									<Button
+										variant="contained"
+										color="primary"
+										className={classes.button}
+										type="submit"
+									>
+										送出
+									</Button>
+								</form>
+							</Paper>
+						</ClickAwayListener>
 					</Backdrop>
 
 					<Grid item className={classes.info}>
