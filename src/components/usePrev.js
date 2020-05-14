f9be3e4e61d63855b6ab1ea7ebf63b98bf12acf0
ipodtouch64g <qazwyt3196@gmail.com>
2020-05-14 21:27:47 +0800

    clean

--- src/components/usePrev.js
+++
@@ -1,8 +0,0 @@
-import {React, useRef,useEffect} from "react";
-export default function usePrev(value) {
-    const ref = useRef();
-    useEffect(() => {
-      ref.current = value;
-    });
-    return ref.current;
-  }
