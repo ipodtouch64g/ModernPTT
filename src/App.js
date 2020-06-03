e774eccd2c9e62309678f408b08c25188dab5d96
ipodtouch64g <qazwyt3196@gmail.com>
2020-06-03 22:18:42 +0800

    check is chrome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Main from "./components/Main";
+import Bowser from "bowser";
 
-class App extends Component {
-	render() {
-		return <Main />;
-	}
+export default function App(props) {
+	const notChrome = (
+		<span>
+			Due to technical issue(cannot set custom Websocket Origin header).
+			At this time, we only support Chrome.{" "}
+			<span role="img" aria-label="Person Bowing">
+				🙇🙇🙇🙇🙇
+			</span>{" "}
+		</span>
+	);
+
+	const isChrome = () => {
+		// check is chrome
+		const browser = Bowser.getParser(window.navigator.userAgent);
+		return browser.satisfies({
+			desktop: {
+				chrome: ">=80"
+			}
+		});
+	};
+
+	return (isChrome() ? <Main /> : notChrome);
+	
 }
-
-export default App;
